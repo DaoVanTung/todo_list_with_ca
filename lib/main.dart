af82bb4a73710e497007b503c412fc65c818e72c
Chuyen Trung Tran <chuyentt@gmail.com>
2023-07-28 01:55:16 +0700

    Cập nhật Injector để tích hợp các lớp được tốt hơn

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'src/data/data_sources/todo_data_source.dart';
-import 'src/data/repositories/todo_repository_impl.dart';
-import 'src/domain/repositories/todo_repository.dart';
-import 'src/domain/use_cases/todo_use_case.dart';
-import 'src/presentation/change_notifiers/todo_change_notifier.dart';
+import 'injector.dart';
 import 'src/presentation/pages/todo_list_page.dart';
 
 void main() {
@@ -16,10 +12,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final TodoDataSource todoDataSource = TodoDataSource();
-    final TodoRepository todoRepository = TodoRepositoryImpl(todoDataSource);
-    final TodoUseCase todoUseCase = TodoUseCase(todoRepository);
-    final todoChangeNotifier = TodoChangeNotifier(todoUseCase);
+    final injector = Injector();
+    final todoChangeNotifier = injector.provideTodoChangeNotifier();
 
     return MaterialApp(
       title: 'Flutter Demo',
